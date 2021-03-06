e098583f33a7c1e479d78fec308b890ab88b383d
new_user <new_user@gmail.com>
2021-03-06 13:55:03 +0200

    Добавьте файлы проекта.

---
+++ TestStaticFields/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStaticFields
+{
+    class Config
+    {
+        public static string Token;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable.Repeat("Hello", 5)
+                .Select(q =>
+                {
+                    return new Task(() =>
+                    {
+                        for (int i = 0; i < 10000; i++)
+                        {
+                            Config.Token = q;
+                            Console.WriteLine(i);
+                            Console.WriteLine(Config.Token);
+                        }
+                    },TaskCreationOptions.LongRunning);
+                })
+                .ToList()
+                .ForEach(h => h.Start());
+            Console.ReadKey();
+
+        }
+    }
+}
